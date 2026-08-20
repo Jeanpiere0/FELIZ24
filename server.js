bcb402462552e21ca6002168a0e587d1716c7254
Jeanpiere0 <jeanpiere.surco@unmsm.edu.pe>
2026-08-20 16:24:34 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/feliz24.html");
+});
+
+app.post("/visita", (req, res) => {
+
+    const ip =
+        req.headers["x-forwarded-for"]?.split(",")[0].trim() ||
+        req.socket.remoteAddress;
+
+    const visita = {
+        ip: ip,
+        fecha: new Date().toLocaleString("es-PE"),
+        navegador: req.body.navegador || "Desconocido",
+        idioma: req.body.idioma || "Desconocido",
+        pantalla: req.body.pantalla || "Desconocida",
+        zonaHoraria: req.body.zonaHoraria || "Desconocida"
+    };
+
+    console.log("\n===== NUEVA VISITA =====");
+    console.log(visita);
+
+    fs.appendFileSync(
+        "visitas.txt",
+        JSON.stringify(visita) + "\n"
+    );
+
+    res.json({ ok: true });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
